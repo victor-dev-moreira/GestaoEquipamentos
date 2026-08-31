188e96590630db8dd9bdb21c17070f43d75531df
Victor <victor_hugo_moreira@hotmail.com>
2026-08-31 19:23:29 -0300

    Ajuste no ViewModels

--- GestaoDeEquipamentos.WebApplication/Modulos/Equipamentos/Apresentacao/EquipamentoViewModels.cs
+++ GestaoDeEquipamentos.WebApplication/Modulos/Equipamentos/Apresentacao/EquipamentoViewModels.cs
@@ -45,7 +45,8 @@ public record EditarEquipamentoViewModel
     string? Nome,
 
     [Required(ErrorMessage = "O campo \"Preço de aquisição\" é obrigatório.")]
-    [Range(typeof(decimal), "0,01", "79228162514264337593543950335",
+    [Range(typeof(decimal), "0.01", "79228162514264337593543950335",
+    ParseLimitsInInvariantCulture = true,
         ErrorMessage = "O campo \"Preço de aquisição\" deve ser maior que zero.")]
     decimal? PrecoAquisicao,
 
